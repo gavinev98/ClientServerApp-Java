d13052dca3722385768bb756dd6de19f5835e93a
gavinev98 <gavin.everett48@hotmail.com>
2019-03-07 10:26:21 +0000

    Added file directory to store users session.

--- ftpserver/src/main/java/com/gavineverett/server/EchoServer1.java
+++ ftpserver/src/main/java/com/gavineverett/server/EchoServer1.java
@@ -1,6 +1,7 @@
 package com.gavineverett.server;
 
 import java.io.File;
+import java.io.IOException;
 
 public class EchoServer1 {
     //defining codes
@@ -8,20 +9,20 @@ public class EchoServer1 {
     public static final String logout = "204";
     public static final String fileUpload = "300";
     public static final String fileDownload = "500";
+    static File userDirectory;
 
     public static void main(String[] args) {
 
         //creating a directory to store users
-        File userDirectory = new File("C:\\ServerSession");
+        userDirectory = new File("C:\\ServerSession");
         //check if directory does not exist.
-        if(!userDirectory.exists())
-        {
+        if (!userDirectory.exists()) {
             userDirectory.mkdir();
         }
 
 
         int serverPort = 7;    // default port
-        if (args.length == 1 )
+        if (args.length == 1)
             serverPort = Integer.parseInt(args[0]);
         try {
             // instantiates a datagram socket for both sending
@@ -32,24 +33,28 @@ public class EchoServer1 {
                 DatagramMessage request =
                         mySocket.receiveMessageAndSender();
                 System.out.println("Request received");
-                String message = request.getMessage( );
-                System.out.println("message received: "+ message);
+                String message = request.getMessage();
+                System.out.println("message received: " + message);
                 // Now send the echo to the requestor
-                mySocket.sendMessage(request.getAddress( ),
-                        request.getPort( ), message);
+                mySocket.sendMessage(request.getAddress(),
+                        request.getPort(), message);
 
 
                 //when the message is retrieved the client will send back a code followed by the request of login.
-                  String code = message.substring(0, 3);
+                String code = message.substring(0, 3);
 
                 //create a switch statement to decipher between the codes chosen.
-                switch (code)
-                {
+                switch (code) {
                     case login:
                         //logincode
                         String recieveMessage = request.getMessage();
                         String extractUserName = recieveMessage.substring(14, 19);
                         System.out.println("The username is: " + extractUserName);
+                        //check for users validity.
+                        performLoginOperation(extractUserName);
+                        //send back relevant information back to the client.
+                        mySocket.sendMessage(request.getAddress(),
+                                request.getPort(), extractUserName);
                         break;
 
 
@@ -63,7 +68,6 @@ public class EchoServer1 {
                         break;
 
 
-
                     case fileDownload:
                         //filedownload
                         break;
@@ -74,16 +78,36 @@ public class EchoServer1 {
             } //end while
         } // end try
         catch (Exception ex) {
-            ex.printStackTrace( );
+            ex.printStackTrace();
         } // end catch
     } //end main
 
 
     //methods to perform actions.
 
-    public String performLoginOperation(String username)
-    {
-        return null;
+    public static String performLoginOperation(String username) {
+        try {
+            //check for txt file
+            final String usertxt = username;
+            final String filename = username + ".txt";
+            final File file = new File("C:\\ServerSession", filename);
+            //boolean variable if user exists
+            boolean userExists = file.exists();
+            // check if user exists by checking file directory.
+            if (userExists) {
+                String auth = "Welcome back to the system" + username;
+                return auth;
+            } else {
+                //If user does not exist create a new file for the user and add to directory.
+                file.createNewFile();
+                String registered = "You have been successfully registered " + username + " enjoy!";
+                //return string registered if user has successfully been registered.
+                return registered;
+            }
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+       return null;
     }
-
-} // end class
+}
+    // end class
